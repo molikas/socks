604e435478ddc21b03a94f22d34da8659a325dec
molikas <molikas@gmail.com>
2016-03-29 12:36:34 +0300

    Initial code

---
+++ socks-core/src/main/java/com/socks/SocksCoreApplication.java
@@ -0,0 +1,12 @@
+package com.socks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocksCoreApplication {
+
+		public static void main(String[] args) {
+			SpringApplication.run(SocksCoreApplication.class, args);
+		}
+}
